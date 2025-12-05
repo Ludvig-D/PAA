64bd25c039d61735dafc8080be1222405eb056c2
Ludvig <74721111+Ludvig-D@users.noreply.github.com>
2025-12-05 13:22:50 +0100

    Setup router strukture

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router';
 import './App.css';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
