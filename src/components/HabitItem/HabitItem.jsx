de74a508d5ea9cbdf0ea8a34c2009903eaad855d
Ludvig <74721111+Ludvig-D@users.noreply.github.com>
2025-12-06 14:11:31 +0100

    Added basic style

--- src/components/HabitItem/HabitItem.jsx
+++ src/components/HabitItem/HabitItem.jsx
@@ -1,11 +1,13 @@
+import style from './HabitItem.module.css';
+
 export default function HabitItem({ habit }) {
   return (
-    <div>
-      <div>
-        <p>{habit.title}</p>
-        <p>{habit.priority}</p>
+    <div id={style.item}>
+      <div id={style.firstRow}>
+        <p id={style.title}>{habit.title}</p>
+        <p id={style.priority}>{habit.priority}</p>
       </div>
-      <p>{habit.repetisions}</p>
+      <p id={style.rep}>{habit.repetisions}</p>
     </div>
   );
 }
