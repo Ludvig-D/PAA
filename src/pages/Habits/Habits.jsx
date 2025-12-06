ce6e642c396b9390a292414dcb9ce6ae65cec27e
Ludvig <74721111+Ludvig-D@users.noreply.github.com>
2025-12-06 15:09:34 +0100

    Added filter habits

--- src/pages/Habits/Habits.jsx
+++ src/pages/Habits/Habits.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import HabitsForm from '../../components/HabitsForm/HabitsForm';
 import { HabitContext } from '../../context/HabitContext';
 import HabitItem from '../../components/HabitItem/HabitItem';
@@ -6,13 +6,31 @@ import HabitItem from '../../components/HabitItem/HabitItem';
 export default function Habits() {
   const { habits } = useContext(HabitContext);
 
+  const [habitList, setHabitList] = useState(habits);
+
+  const filterHabits = (filter) => {
+    const list = [...habits];
+    const newList = list.filter((habit) => habit.priority === filter);
+    setHabitList(newList);
+  };
+
   return (
     <>
       <p>Habits</p>
       <HabitsForm />
 
+      <select
+        name="filter"
+        id="filter"
+        onChange={(e) => filterHabits(e.target.value)}
+      >
+        <option value="low">Low</option>
+        <option value="medium">Medium</option>
+        <option value="high">High</option>
+      </select>
+
       <div>
-        {habits.map((habit) => (
+        {habitList.map((habit) => (
           <HabitItem habit={habit} />
         ))}
       </div>
