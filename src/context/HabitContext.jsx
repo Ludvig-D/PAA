e895c8846b84f2eda992a9c438c453e880df027a
Ludvig <74721111+Ludvig-D@users.noreply.github.com>
2025-12-07 17:36:16 +0100

    Moved sort and filter to context

--- src/context/HabitContext.jsx
+++ src/context/HabitContext.jsx
@@ -7,6 +7,13 @@ const HabitProvider = ({ children }) => {
     JSON.parse(localStorage.getItem('habits')) || []
   );
 
+  useEffect(() => {
+    localStorage.setItem('habits', JSON.stringify(allHabits));
+  }, [allHabits]);
+
+  const [sort, setSort] = useState('default');
+  const [habitList, setHabitList] = useState(allHabits);
+
   const addNewHabit = (newHabit) => {
     setAllHabits((prev) => [...prev, newHabit]);
   };
@@ -34,17 +41,77 @@ const HabitProvider = ({ children }) => {
     setAllHabits(newHabits);
   };
 
-  useEffect(() => {
-    localStorage.setItem('habits', JSON.stringify(allHabits));
-  }, [allHabits]);
+  const filterHabits = (filter) => {
+    const list = [...allHabits];
+    if (filter === 'all') {
+      setHabitList(list);
+      sortHabits(sort);
+      return;
+    }
+    const newList = list.filter((habit) => habit.priority === filter);
+    setHabitList(newList);
+    sortHabits(sort);
+  };
+
+  const sortHabits = (sort) => {
+    setSort(sort);
+    if (sort === 'default') {
+      setHabitList((prev) =>
+        [...prev].sort((a, b) => {
+          return allHabits.indexOf(a) - allHabits.indexOf(b);
+        })
+      );
+    } else if (sort === 'repsfall') {
+      setHabitList((habits) =>
+        [...habits].sort((a, b) => a.repetisions - b.repetisions)
+      );
+    } else if (sort === 'repsrise') {
+      setHabitList((habits) =>
+        [...habits].sort((a, b) => b.repetisions - a.repetisions)
+      );
+    } else if (sort === 'priorise') {
+      const order = {
+        high: 1,
+        medium: 2,
+        low: 3,
+      };
+
+      setHabitList((prev) =>
+        [...prev].sort((a, b) => {
+          const rankA = order[a.priority];
+          const rankB = order[b.priority];
+
+          return rankA - rankB;
+        })
+      );
+    } else if (sort === 'priofall') {
+      const order = {
+        low: 1,
+        medium: 2,
+        high: 3,
+      };
+
+      setHabitList((prev) =>
+        [...prev].sort((a, b) => {
+          const rankA = order[a.priority];
+          const rankB = order[b.priority];
+
+          return rankA - rankB;
+        })
+      );
+    }
+  };
 
   return (
     <HabitContext
       value={{
         allHabits,
+        habitList,
         addNewHabit,
         removeHabit,
         handelHabitRep,
+        filterHabits,
+        sortHabits,
       }}
     >
       {children}
--- src/pages/Habits/Habits.jsx
+++ src/pages/Habits/Habits.jsx
@@ -1,107 +1,17 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext } from 'react';
 import HabitsForm from '../../components/HabitsForm/HabitsForm';
 import { HabitContext } from '../../context/HabitContext';
 import HabitItem from '../../components/HabitItem/HabitItem';
+import HabitSelects from '../../components/HabitSelects/HabitSelects';
 
 export default function Habits() {
-  const { allHabits } = useContext(HabitContext);
-
-  const [habitList, setHabitList] = useState(allHabits);
-  const [sort, setSort] = useState('default');
-
-  const addToHabitList = (newHabit) => {
-    setHabitList((prev) => [...prev, newHabit]);
-  };
-
-  const filterHabits = (filter) => {
-    const list = [...allHabits];
-    if (filter === 'all') {
-      setHabitList(list);
-      sortHabits(sort);
-      return;
-    }
-    const newList = list.filter((habit) => habit.priority === filter);
-    setHabitList(newList);
-    sortHabits(sort);
-  };
-
-  const sortHabits = (sort) => {
-    setSort(sort);
-    if (sort === 'default') {
-      setHabitList((prev) =>
-        [...prev].sort((a, b) => {
-          return allHabits.indexOf(a) - allHabits.indexOf(b);
-        })
-      );
-    } else if (sort === 'repsfall') {
-      setHabitList((habits) =>
-        [...habits].sort((a, b) => a.repetisions - b.repetisions)
-      );
-    } else if (sort === 'repsrise') {
-      setHabitList((habits) =>
-        [...habits].sort((a, b) => b.repetisions - a.repetisions)
-      );
-    } else if (sort === 'priorise') {
-      const order = {
-        high: 1,
-        medium: 2,
-        low: 3,
-      };
-
-      setHabitList((prev) =>
-        [...prev].sort((a, b) => {
-          const rankA = order[a.priority];
-          const rankB = order[b.priority];
-
-          return rankA - rankB;
-        })
-      );
-    } else if (sort === 'priofall') {
-      const order = {
-        low: 1,
-        medium: 2,
-        high: 3,
-      };
-
-      setHabitList((prev) =>
-        [...prev].sort((a, b) => {
-          const rankA = order[a.priority];
-          const rankB = order[b.priority];
-
-          return rankA - rankB;
-        })
-      );
-    }
-  };
+  const { habitList } = useContext(HabitContext);
 
   return (
     <>
-      <p>Habits</p>
-      <HabitsForm addHabit={addToHabitList} />
-
-      <select
-        name="filter"
-        id="filter"
-        onChange={(e) => filterHabits(e.target.value)}
-      >
-        <option value="all">All</option>
-        <option value="low">Low</option>
-        <option value="medium">Medium</option>
-        <option value="high">High</option>
-      </select>
-
-      <select
-        name="sort"
-        id="sort"
-        onChange={(e) => sortHabits(e.target.value)}
-      >
-        <option value="default">Default</option>
-        <option value="repsfall">Reps Fall</option>
-        <option value="repsrise">Reps Rise</option>
-        <option value="priofall">Prio Fall</option>
-        <option value="priorise">Prio Rise</option>
-      </select>
-
+      <h1>Habits</h1>
+      <HabitsForm />
+      <HabitSelects />
       <div>
         {habitList.map((habit) => (
           <HabitItem key={habit.id} habit={habit} />
