11bf9bcd66e0cddfff94fb49c36d29354771d797
Ludvig <74721111+Ludvig-D@users.noreply.github.com>
2025-12-19 12:56:19 +0100

    Added back btn to habits

--- src/pages/Habits/Habits.jsx
+++ src/pages/Habits/Habits.jsx
@@ -4,6 +4,7 @@ import { HabitContext } from '../../context/HabitContext';
 import HabitItem from '../../components/HabitItem/HabitItem';
 import HabitSelects from '../../components/HabitSelects/HabitSelects';
 import style from './Habits.module.css';
+import { useNavigate } from 'react-router';
 
 export default function Habits() {
   const { habitList } = useContext(HabitContext);
@@ -14,9 +15,19 @@ export default function Habits() {
     setHideCreate((prev) => !prev);
   };
 
+  const nav = useNavigate();
+  const handleNavBack = () => {
+    nav('/');
+  };
+
   return (
     <div id={style.mainContainer}>
       <h1>Habits</h1>
+      <div className={style.backBtnDiv}>
+        <button className={style.backBtn} onClick={handleNavBack}>
+          {'<-- Back'}
+        </button>
+      </div>
       <div id={style.test}>
         <button id={style.hideBtn} onClick={() => toggleHide()}>
           Add habit
