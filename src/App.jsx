f4ef71569419e40305c36b0398adc33bfdb7902c
Ludvig <74721111+Ludvig-D@users.noreply.github.com>
2025-12-19 12:02:52 +0100

    Removed route auth

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,11 @@ import Home from './pages/Home';
 import EventPage from './pages/Events/EventPage.jsx';
 import Habits from './pages/Habits/Habits';
 import TodoListPage from './pages/TodoListPage';
-
 import TodoForm from './pages/TodoForm';
-import Auth from './components/Auth/Auth.jsx';
 
 function App() {
   return (
     <>
-      <h1>hej</h1>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />;
@@ -19,7 +16,6 @@ function App() {
           <Route path="todos" element={<TodoListPage />} />;
           <Route path="/form" element={<TodoForm />} />;
           <Route path="/event" element={<EventPage />} />;
-          <Route path="/auth" element={<Auth />} />;
         </Routes>
       </BrowserRouter>
     </>
