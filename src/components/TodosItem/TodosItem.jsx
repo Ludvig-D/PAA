34219de8c99b713b87f1e12ab6899933ee2358cb
Taif Helly <taifhelly@hotmail.com>
2025-12-22 16:12:50 +0100

    styling

--- src/components/TodosItem/TodosItem.jsx
+++ src/components/TodosItem/TodosItem.jsx
@@ -1,40 +1,45 @@
-import {useContext} from 'react'
+import { useContext } from 'react';
+import style from './todosItem.module.css';
 import { TodoContext } from '../../context/TodoContext';
-export default function todosItem({todo, handleEdit, list}) {
-const { todos, deleteTodo, toggleStatus } = useContext(TodoContext); 
-    return (<div >
-        <h3>{todo.title}</h3>
-        <p>{todo.description}</p>
-        <p>{todo.category}</p>
-        <p>{todo.deadline}</p>
-        <p>{todo.timeEstimate} min</p>
 
-        <div>
-            <label>
-                <input
-                    type="checkbox"
-                    checked={todo.status}
-                    onChange={() => toggleStatus(todo.id)}
-                />
-                <span>
-                    {todo.status ? 'slutförd' : 'ej slutförd'}
-                </span>
-            </label>
+export default function TodosItem({ todo, handleEdit, list }) {
+    const { deleteTodo, toggleStatus } = useContext(TodoContext);
+
+    return (
+        <div className={style.item}>
+            <div>
+                <p className={style.title}>{todo.title}</p>
+                <p>{todo.description}</p>
+                <p className={`${style.tag} ${style[todo.priority]}`}>{todo.priority}</p>
+                <p>{todo.category}</p>
+                <p>{todo.deadline}</p>
+                <p>{todo.timeEstimate} min</p>
+                <div>
+                    <label>
+                        <input
+                            type="checkbox"
+                            checked={todo.status}
+                            onChange={() => toggleStatus(todo.id)}
+                        />
+                        <span>
+                            {todo.status ? 'slutförd' : 'ej slutförd'}
+                        </span>
+                    </label>
+                </div>
+            </div>
+            <div className={style.removeBtnDiv}>
+                {list && (
+                    <button onClick={() => handleEdit(todo)} className={style.removeBtn}>
+                        Redigera
+                    </button>
+                )}
+                <button onClick={() => deleteTodo(todo.id)} className={style.removeBtn}>
+                    Ta bort
+                </button>
+            </div>
         </div>
-
-        {list && <button
-            onClick={() => handleEdit(todo)}
-            style={{
-                //kan styla något härs
-            }}
-        >
-            Redigera
-        </button>}
-
-        <button onClick={() => deleteTodo(todo.id)}>
-            Ta bort
-        </button>
-    </div>)
+    );
 }
+
 
 
